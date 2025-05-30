4f24e10b3ddec7a8073224ef0402758721ecd4de
Fatema Akther <sumafatema10@gmail.com>
2025-05-30 17:31:39 +0600

    Initial type error

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,10 +27,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
 
-              -                         cz-shortcut-listen="true" 
-              -                         cz-shortcut-listen="true"
-              -                         cz-shortcut-listen="true"
-
+            
       </body>
     </html>
   );
